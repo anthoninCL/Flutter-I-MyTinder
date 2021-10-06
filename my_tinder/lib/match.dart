c12e4f96ee1dae06706ada2ff486ea7e8d4d66a9
mattéo <fauchonmatteo@gmail.com>
2021-10-06 18:10:01 +0200

    feat: add navigation bar

--- my_tinder/lib/match.dart
+++ my_tinder/lib/match.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:my_tinder/Views/matches/matches.dart';
+import 'package:my_tinder/Views/messages/messages.dart';
+import 'package:my_tinder/Views/profile/profile.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
@@ -25,13 +30,39 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  int _selectedIndex = 0;
+  final screens = [Matches(), Messages(), Profile()];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       body: Center(
-          child: Text("Hello")
+          child: IndexedStack(
+            index: _selectedIndex,
+            children: screens,
+          )
       ),
+      bottomNavigationBar: _bottomNavigationBar(),
     );
   }
+
+  Widget _bottomNavigationBar() => BottomNavigationBar(
+    items: [
+      BottomNavigationBarItem(icon: _selectedIndex == 0 ? SvgPicture.asset("assets/icons/main-icon.svg", height: 22) : SvgPicture.asset("assets/icons/main-icon-outline.svg", height: 22), label: ""),
+      BottomNavigationBarItem(icon: _selectedIndex == 1 ? const Icon(FontAwesomeIcons.solidCommentDots) : const Icon(FontAwesomeIcons.commentDots), label: ""),
+      BottomNavigationBarItem(icon: _selectedIndex == 2 ? const Icon(Icons.person) : const Icon(Icons.person_outline), label: "")
+    ],
+    currentIndex: _selectedIndex,
+    onTap: _onItemTapped,
+    backgroundColor: Colors.black.withOpacity(0.4),
+    elevation: 1,
+    selectedItemColor: Colors.white,
+    unselectedItemColor: const Color(0xCCCCCCCC),
+  );
 }
