10d1d5027e3d27d032f65d0d8671190ecfbb6d0f
Anthonin Clara <anthonin.clara@epitech.eu>
2021-10-09 20:24:28 +0200

    feat: add the circle avatar widget

--- my_tinder/lib/views/messages/messages.dart
+++ my_tinder/lib/views/messages/messages.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:my_tinder/shared/widgets/clickable_avatar.dart';
 
 class Messages extends StatefulWidget {
   const Messages({Key? key}) : super(key: key);
@@ -8,10 +9,26 @@ class Messages extends StatefulWidget {
 }
 
 class _MessagesState extends State<Messages> {
+
+  void printHello() {
+    // ignore: avoid_print
+    print("hello");
+  }
+
   @override
   Widget build(BuildContext context) {
+    precacheImage(const AssetImage("assets/images/profile_picture_example.jpeg"), context);
     return Scaffold(
-      body: Center(child: Text("Messages")),
+      body: SafeArea(
+        child: Center(
+            child: ClickableAvatar(
+              image: "assets/images/profile_picture_example.jpeg",
+              radius: 30,
+              name: "Léa",
+              onTap: printHello,
+            )
+        ),
+      ),
     );
   }
 }
