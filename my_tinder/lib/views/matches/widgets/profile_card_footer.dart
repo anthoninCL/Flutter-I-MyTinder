c934b3ec211d0ae8326fae928ab4a5a14a032ee3
mattéo <fauchonmatteo@gmail.com>
2021-10-11 14:41:08 +0200

    feat: add delete and like buttons in the matches screen

--- my_tinder/lib/views/matches/widgets/profile_card_footer.dart
+++ my_tinder/lib/views/matches/widgets/profile_card_footer.dart
@@ -1,17 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:my_tinder/constants/matches.dart';
 import 'package:scroll_to_index/scroll_to_index.dart';
 
 class ProfileCardFooter extends StatelessWidget {
-  final profile;
-  final index;
+  final Match profile;
+  final int index;
   final controller;
-  const ProfileCardFooter({Key? key, required this.profile, required this.index, required this.controller}) : super(key: key);
+
+  const ProfileCardFooter(
+      {Key? key,
+      required this.profile,
+      required this.index,
+      required this.controller})
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: EdgeInsets.only(
-          bottom: MediaQuery.of(context).size.height / 7),
+      padding: EdgeInsets.only(bottom: MediaQuery.of(context).size.height / 7),
       child: SizedBox(
         width: MediaQuery.of(context).size.width - 20,
         child: Padding(
@@ -20,11 +26,10 @@ class ProfileCardFooter extends StatelessWidget {
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               buildProfileDescription(
-                  name: "Anne Laure",
-                  age: 25,
-                  description:
-                  "Travel ✈️ Sport and food, Only god can judge me..."),
-              //buildChoiceRow(index: index),
+                  name: profile.name,
+                  age: profile.age,
+                  description: profile.bio),
+              buildChoiceRow(index: index),
             ],
           ),
         ),
@@ -32,25 +37,53 @@ class ProfileCardFooter extends StatelessWidget {
     );
   }
 
-  Widget buildProfileDescription({required String name, required int age, required String description}) => Column(
-    crossAxisAlignment: CrossAxisAlignment.start,
-    children: [
-      Text(name + "," + age.toString(), style: const TextStyle(fontSize: 27, fontWeight: FontWeight.bold, color: Colors.white),),
-      const SizedBox(height: 10),
-      Text(description, style: const TextStyle(fontSize: 16, color: Colors.white),),
-      const SizedBox(height: 20),
-    ],
-  );
+  Widget buildProfileDescription(
+          {required String name,
+          required num age,
+          required String description}) =>
+      Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            name + ", " + age.toString(),
+            style: const TextStyle(
+                fontSize: 27, fontWeight: FontWeight.bold, color: Colors.white),
+          ),
+          const SizedBox(height: 10),
+          Text(
+            description,
+            style: const TextStyle(fontSize: 16, color: Colors.white),
+          ),
+          const SizedBox(height: 20),
+        ],
+      );
 
   Future _scrollToIndex(index) async {
-    await controller.scrollToIndex(index + 1, preferPosition: AutoScrollPosition.begin);
+    await controller.scrollToIndex(index + 1,
+        preferPosition: AutoScrollPosition.begin);
   }
 
-  /*Widget buildChoiceRow({required index}) => Row(
-    mainAxisAlignment: MainAxisAlignment.spaceAround,
-    children: [
-      myFAB(icon: const Icon(Icons.close, size: 30,), onPress: () => _scrollToIndex(index),),
-      myFAB(icon: const Icon(Icons.favorite_outline, size: 30, color: Colors.red,), onPress: () => _scrollToIndex(index)),
-    ],
-  );*/
+  Widget buildChoiceRow({required index}) => Row(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: [
+          FloatingActionButton(
+            onPressed: () => _scrollToIndex(index),
+            child: const Icon(
+              Icons.close,
+              size: 30,
+              color: Colors.black,
+            ),
+            backgroundColor: Colors.white,
+          ),
+          FloatingActionButton(
+            onPressed: () => _scrollToIndex(index),
+            child: const Icon(
+              Icons.favorite_outline,
+              size: 30,
+              color: Colors.red,
+            ),
+            backgroundColor: Colors.white,
+          ),
+        ],
+      );
 }
